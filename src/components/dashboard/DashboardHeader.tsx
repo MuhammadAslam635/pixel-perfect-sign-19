3a181396d5a6fa88573823d289161973a7793881
zubairkofficial <thezubairkhan.developer@gmail.com>
2025-11-11 16:23:26 +0500

    make logo consistent in dashboard

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,7 @@
 
 import { ActionComponent } from './ActionComponent';
 import { Navigation } from './Navigation';
-import { LogoComponent } from './LogoComponent';
+import Logo from '../Logo';
 
 
 
@@ -11,7 +11,7 @@ const DashboardHeader = () => {
   return (
     <header className="fixed top-3 left-0 right-0 z-50 flex h-14 items-center gap-4 rounded-full bg-transparent px-0 pt-2">
       <div className="px-5 flex w-full items-center justify-between sm:px-8 lg:px-16 xl:px-[66px]">
-      <LogoComponent />
+      <Logo />
       <Navigation />
 
       <ActionComponent />
